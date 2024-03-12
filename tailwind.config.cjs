ea29a9332168b52d02d310728c345a4f39f2c207
EliasBallen <@EliasBallen>
2024-03-12 08:47:29 -0500

    initial portfolio, need to fix transition to proyects page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,17 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: [{cvCustomTheme:{
+			"primary": "#00c18b",
+			"secondary": "#00cdee",
+			"accent": "#0e7490",
+			"neutral": "#111827",
+			"base-100": "#374151",
+			"info": "#00ccff",
+			"success": "#00c18b",
+			"warning": "#f59e0b",		 
+			"error": "#dc2626",
+		}},"dark","emerald"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
